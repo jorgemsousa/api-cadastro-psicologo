cb50c2aecca3b62e57d7fae1a78d1fffc7b8df03
Jorge Sousa <jorgeferartsousa@gmail.com>
2022-05-16 20:31:59 -0300

    response all users psychologisty with no body

--- src/modules/psychologistUser/useCases/getAllPsychologistUser/GetAllPsychologistUserUseCase.ts
+++ src/modules/psychologistUser/useCases/getAllPsychologistUser/GetAllPsychologistUserUseCase.ts
@@ -1,9 +1,14 @@
 import { PsychologistUser } from "@prisma/client";
+import { AppError } from "../../../../errors/AppError";
 import { prisma } from "../../../../prisma/client";
 
 export class GetAllPsychologistUserUseCase {
   async execute(): Promise<PsychologistUser[]>{
     const getAllPsychologistUsers = await prisma.psychologistUser.findMany({});
+
+    if(getAllPsychologistUsers.length <= 0) {
+      throw new AppError("PsychologistUsers do not found", 204);      
+    }
 
     return getAllPsychologistUsers;
   }
